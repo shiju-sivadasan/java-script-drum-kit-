44dff8633dc1931032f530102af7fb9275007609
shiju sivadasan <134185743+shiju-sivadasan@users.noreply.github.com>
2024-01-19 16:44:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener("keydown", function (e) {
+    //console.log(e);
+    //console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    key.classList.add("playing");
+    console.log(audio);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+});
